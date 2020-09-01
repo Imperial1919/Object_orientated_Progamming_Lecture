f49545784a138d8dd34978c6c7039181082c8d3c
Bryce <ImperialEagle1998@gmail.com>
2020-09-01 13:25:33 -0500

    Add project files.

---
+++ Object_orientated_Progamming_Lecture/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Text;
+
+namespace classiccar
+{
+    public class classic_car 
+    {
+        public string make { get; set; }
+        public string model { get; set; }
+        public int year { get; set; }
+
+
+
+        public classic_car()
+        {
+            
+        }
+
+
+
+        
+    }
+}
---
+++ Object_orientated_Progamming_Lecture/Program.cs
@@ -0,0 +1,26 @@
+﻿using classiccar;
+using System;
+
+namespace Object_orientated_Progamming_Lecture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            classic_car Cadillac_S62 = new classic_car();
+            {
+
+                Cadillac_S62.make = "Cadillac Motor Company";
+                Cadillac_S62.model = "Series 62 Convertible";
+                Cadillac_S62.year = 1947;
+                Console.WriteLine($"Car Model Series: {Cadillac_S62.model}");
+                
+                Console.WriteLine($"Make: {Cadillac_S62.make}");
+
+                Console.WriteLine($"Year Built: {Cadillac_S62.year}");
+            }
+        }
+    }
+}
+
+
